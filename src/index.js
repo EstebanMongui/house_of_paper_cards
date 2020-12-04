0d62fd68d690a7bd8530f815fbedbc021672b085
Esteban Monguí <esteband.itorres@gmail.com>
2020-12-04 08:01:03 -0500

    refact: clear and customize files for this project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
+//React
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//Components
+import ProfessionalCard from "./components/ProfessionaCard"
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//Containers
+const container = document.getElementById('root');
+
+ReactDOM.render(<ProfessionalCard 
+    characterName="Tokio" 
+    firstName="Úrsula" 
+    lastName="Corveró Delgado"
+    description="“La Casa de Papel ha sido un viaje maravilloso.
+    Es uno de los proyectos que más me han enseñado
+    y con los que más he disfrutado.”"
+    avatarUrl = "https://i.pinimg.com/originals/ab/02/a8/ab02a827820743f73186a3e6eeddbe7f.jpg"
+    articleUrl = "https://la-casa-de-papel.fandom.com/es/wiki/%C3%9Arsula_Corber%C3%B3"
+    imgCreditsUrl = "https://www.talenthouse.com/item/2253716/695e99bb"
+    imgCredits = "Wawo Murillo"
+    />, 
+    container);
